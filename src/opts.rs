a6c7bde9105ce077e8bbcd1175b88b798c9bb8a6
马锦峰 <majf.colt@gmail.com>
2024-07-01 22:29:47 +0800

    support generic  csv to json,yaml output

--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+use std::str::FromStr;
+
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -18,14 +21,50 @@ pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
+
+    #[arg(long, default_value = "json")]
+    pub format: OutputFormat,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("unsupported format")),
+        }
+    }
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
+    }
 }
 
 fn verify_input_file(file_name: &str) -> Result<String, &'static str> {
--- src/process.rs
+++ src/process.rs
@@ -2,27 +2,37 @@ use std::fs;
 
 use anyhow::Result;
 use csv::Reader;
-use serde::{Deserialize, Serialize};
+// use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+use crate::opts::OutputFormat;
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+// #[derive(Debug, Deserialize, Serialize)]
+// #[serde(rename_all = "PascalCase")]
+// struct Player {
+//     name: String,
+//     position: String,
+//     #[serde(rename = "DOB")]
+//     dob: String,
+//     nationality: String,
+//     #[serde(rename = "Kit Number")]
+//     kit: u8,
+// }
+
+pub fn process_csv(input: &str, output: &str, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let records = reader
-        .deserialize()
-        .map(|record| record.unwrap())
-        .collect::<Vec<Player>>();
-    let json = serde_json::to_string(&records)?;
-    fs::write(output, json)?;
+    let headers = reader.headers()?.clone();
+    let mut result = Vec::with_capacity(128);
+    for record in reader.records() {
+        let record = record?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        result.push(json_value)
+    }
+
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&result)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&result)?,
+    };
+    fs::write(output, content)?;
     Ok(())
 }
